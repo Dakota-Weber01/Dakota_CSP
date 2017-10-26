5eeaf6e5e58dbbb0e2a47db7d2f5de953a217f8a
Dakota Weber <dakota.weber01@gmail.com>
2017-10-26 12:30:23 -0600

    Added cocoa classes to replace the blank swift files and re-added app delegate; formatted the parens and spacing to my preference.

---
+++ Dakota_CSP/Controller/CreativityController.swift
@@ -0,0 +1,21 @@
+//
+//  CreativityController.swift
+//  Dakota_CSP
+//
+//  Created by Weber, Dakota on 10/26/17.
+//  Copyright © 2017 CTEC. All rights reserved.
+//
+
+import UIKit
+
+class CreativityController: UIViewController
+{
+    override func viewDidLoad()
+    {
+        super.viewDidLoad()
+    }
+    override func didReceiveMemoryWarning()
+    {
+        super.didReceiveMemoryWarning()
+    }
+}
