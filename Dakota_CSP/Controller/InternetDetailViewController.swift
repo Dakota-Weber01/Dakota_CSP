9f1996b0333391d9da93eb458008d255be5f2529
Dakota Weber <dakota.weber01@gmail.com>
2017-12-01 13:24:34 -0700

    Finished whole frame for Internet detail view controller

--- Dakota_CSP/Controller/InternetDetailViewController.swift
+++ Dakota_CSP/Controller/InternetDetailViewController.swift
@@ -14,17 +14,54 @@ public class InternetDetailViewController : UIViewController
     //MARK: GUI CONTROLS
     @IBOutlet weak var textView: UILabel!
     @IBOutlet weak var webViewer: WKWebView!
-    
+    var detailAddress : String?
+    {
+        //MARK: Update the detail view when a value is changed.
+        didSet
+            {
+             configureDetailView()
+            }
+    }
+    var detailText : String?
+    {
+        // MARK: Update the detail view when a value is changed.
+        didSet
+        {
+            configureDetailView()
+        }
+    }
     override public func viewDidLoad()
     {
         super.viewDidLoad()
-        setup()
-        // Do any additional setup after loading the view.
+        configureDetailView()
+        // MARK: Do any additional setup after loading the view.
     }
     
-    private func setup() -> Void
+    private func configureDetailView() -> Void
     {
-        
+        if detailAddress != nil
+        {
+            if let currentWebView = webViewer
+            {
+                let currentURL = URL(string: "https://www.cnn.com")
+                let currentWebRequest = URLRequest(url: currentURL!)
+                currentWebView.load(currentWebRequest)
+            }
+        }
+        if detailText != nil
+        {
+            if let currentText = textView
+                {
+                currentText.text = detailText
+                }
+        }
+        else
+        {
+            if let currentText = textView
+            {
+                currentText.text = "Dakota's CSP app internet screen"
+            }
+        }
     }
-    
 }
+
