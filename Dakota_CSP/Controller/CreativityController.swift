76a83967c8164f453152f32eb1ac61d628844164
Dakota Weber <dakota.weber01@gmail.com>
2017-12-07 12:28:18 -0700

    Added delegate method

--- Dakota_CSP/Controller/CreativityController.swift
+++ Dakota_CSP/Controller/CreativityController.swift
@@ -11,7 +11,7 @@ public class CreativityController: UICollectionViewController, UICollectionViewD
 {
     private let reuseIdentifer = "artIdentifier"
     private let sectionInsets = UIEdgeInsets(top: 50.0, left: 20.0, bottom: 50.0, right: 20.0)
-    private let itemsPerRo : CGFloat = 3
+    private let itemsPerRow : CGFloat = 3
     
     private lazy var artSelection : [UIImage?] =
     {
@@ -80,6 +80,26 @@ public class CreativityController: UICollectionViewController, UICollectionViewD
         artCell.imageName.text = "My Art"
         
         return artCell
+    }
+    //MARK:- Delegate methods
+    
+    public func collectionView(_ collectionView: UICollectionViewCell,
+                               sizeforItemAt indexPath: IndexPath) -> CGSize
+    {
+        if indexPath == largePhotoIndexPath
+        {
+            let art = artSelection[indexPath.row]
+            let size = collectionView.bounds.size
+            let widthScale = (size.width / art!.size.width) * CGFloat(0.80)
+            let largeSize = CGSize(width: art!.size.width * widthScale, height: art!.size.height * widthScale)
+            
+            return largeSize
+        }
+        let paddingSpace = sectionInsets.left * (itemsPerRow + 1)
+        let availableWidth = view.frame.width - paddingSpace
+        let widthPerItem = availableWidth / itemsPerRow
+        
+        return CGSize(width: widthPerItem, height: widthPerItem)
     }
     public override func didReceiveMemoryWarning()
     {
