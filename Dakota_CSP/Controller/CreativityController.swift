a40119a91d96c8ed114cd632c02476fe9df0f9ea
Dakota Weber <dakota.weber01@gmail.com>
2017-12-07 11:48:37 -0700

    Made a UImage array

--- Dakota_CSP/Controller/CreativityController.swift
+++ Dakota_CSP/Controller/CreativityController.swift
@@ -7,14 +7,32 @@
 //
 
 import UIKit
-
-class CreativityController: UIViewController
+public class CreativityController: UICollectionViewController, UICollectionViewDelegateFlowLayout
 {
-    override func viewDidLoad()
+    private let reuseIdentifer = "artIdentifier"
+    private let sectionInsets = UIEdgeInsets(top: 50.0, left: 20.0, bottom: 50.0, right: 20.0)
+    private let itemsPerRo : CGFloat = 3
+    
+    private lazy var artSelection : [UIImage?] =
+    {
+        return [
+            UIImage(named: ""),
+            UIImage(named: ""),
+            UIImage(named: ""),
+            UIImage(named: ""),
+            UIImage(named: ""),
+            UIImage(named: ""),
+            UIImage(named: ""),
+            UIImage(named: "")
+               ]
+    }()
+    var largePhotoIdexPath: IndexPath?
+    
+    public override func viewDidLoad()
     {
         super.viewDidLoad()
     }
-    override func didReceiveMemoryWarning()
+    public override func didReceiveMemoryWarning()
     {
         super.didReceiveMemoryWarning()
     }
