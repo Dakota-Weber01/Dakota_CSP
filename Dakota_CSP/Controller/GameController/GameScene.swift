c2ab39ce6947dbd1476b09f23a277b674e80ff67
Dakota Weber <dakota.weber01@gmail.com>
2017-12-19 12:46:40 -0700

    refactored player laser and invader laser from bullet

--- Dakota_CSP/Controller/GameController/GameScene.swift
+++ Dakota_CSP/Controller/GameController/GameScene.swift
@@ -180,11 +180,11 @@ public class GameScene: SKScene, SKPhysicsContactDelegate
             firstBody = contact.bodyB
             secondBody = contact.bodyA
         }
-        if ((firstBody.categoryBitMask & CollisionCategories.Invader != 0) && (secondBody.categoryBitMask & CollisionCategories.PlayerBullet != 0))
+        if ((firstBody.categoryBitMask & CollisionCategories.Invader != 0) && (secondBody.categoryBitMask & CollisionCategories.PlayerLaser != 0))
         {
             print("Invader and Player Bullet Contact")
         }
-        if ((firstBody.categoryBitMask & CollisionCategories.Player != 0) && (secondBody.categoryBitMask & CollisionCategories.InvaderBullet != 0))
+        if ((firstBody.categoryBitMask & CollisionCategories.Player != 0) && (secondBody.categoryBitMask & CollisionCategories.InvaderLaser != 0))
         {
             print("Player and Invader Bullet Contact")
         }
